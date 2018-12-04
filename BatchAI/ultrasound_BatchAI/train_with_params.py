d19f282c41473f3dca845e39252a61af9805e59b
Zenodia <Zenodia@users.noreply.github.com>
2018-12-04 21:40:18 +0100

    add AzureBatchAI hyperparameter tuning techniques
    add ipynb for hyper parameters tuning with Azure BatchAI 
    rename the train.py to train_with_params.py to mark the difference

---
+++ BatchAI/ultrasound_BatchAI/train_with_params.py
@@ -0,0 +1,148 @@
+from __future__ import print_function
+import os
+from skimage.transform import resize
+from skimage.io import imsave
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+import argparse
+from data import load_train_data, load_test_data
+
+K.set_image_data_format('channels_last')  # TF dimension ordering in this code
+
+img_rows = 96
+img_cols = 96
+
+smooth = 1.
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+def get_unet():
+    inputs = Input((img_rows, img_cols, 1))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+    return model
+
+
+def preprocess(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i] = resize(imgs[i], (img_cols, img_rows), preserve_range=True)
+
+    imgs_p = imgs_p[..., np.newaxis]
+    return imgs_p
+
+
+def train_and_predict(data_path):
+    print('-'*30)
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+    imgs_train, imgs_mask_train = load_train_data(data_path)
+
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    print('-'*30)
+    print('Creating and compiling model...')
+    print('-'*30)
+    if load_model=='Y':
+        with open(data_path+'model_architecture.json', 'r') as f:
+            model = model_from_json(f.read())
+            model.load_weights(data_path+'model_weights.h5')
+            print("model loaded ...")
+    else:
+        model = get_unet()
+        print("no previous saved model, create new model")
+    model_checkpoint = ModelCheckpoint(data_path+"epoch"+str(epoch)+"batch_size"+str(batchsize)+'checkpointweights.h5', monitor='val_loss', save_best_only=True)
+
+    print('-'*30)
+    print('Fitting model...')
+    print('-'*30)
+    model.fit(imgs_train, imgs_mask_train, batch_size=batchsize, nb_epoch=epoch, verbose=1, shuffle=True,
+              validation_split=0.2,
+              callbacks=[model_checkpoint])
+    ##### save the modelfrom keras.models import model_from_json, load_model
+
+    # Save Weights + Architecture
+    model.save_weights(data_path+"epoch"+str(epoch)+"batch_size"+str(batchsize)+'model_weights.h5')
+    with open(data_path+"epoch"+str(epoch)+"batch_size"+str(batchsize)+'model_architecture.json', 'w') as f:
+        f.write(model.to_json())
+    
+    print("model saved sucessfully")
+   
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='keras Unet model training')
+    parser.add_argument('--parent', required=True,   help='location of the root directory')
+    parser.add_argument('--load_model', required=True,   help='load previously trained model Y/N')
+    parser.add_argument('--epoch', required=True,   help='number of epoch default to 1')
+    parser.add_argument('--batch_size', required=True,   help='size of batch default to 32')
+    args = parser.parse_args()
+    data_path=args.parent
+    load_model=str(args.load_model)
+    epoch=int(args.epoch)
+    batchsize=int(args.batch_size)
+    print(load_model)
+    train_and_predict(data_path)
